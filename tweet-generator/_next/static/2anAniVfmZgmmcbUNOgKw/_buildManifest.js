7fcf62c40d50b71d32ad9d19444a3054ec5887b4
Prakash Upadhyay <prakash.upadhyay93@yahoo.com>
2022-03-24 17:57:42 +0530

    Delete tweet-generator directory

--- tweet-generator/_next/static/2anAniVfmZgmmcbUNOgKw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/479-d400f4f15d24c7e0.js","static/css/8827abaffcf1a2ce.css","static/chunks/pages/index-512bca0733938d5a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
